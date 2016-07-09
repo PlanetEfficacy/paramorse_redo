66b4a7046e193fbebc1b5faa26f283a47a4c6b2c
Bryan Goss <bcgoss@gmail.com>
2016-07-08 18:23:18 -0600

    Initial Commit! with two test files created

---
+++ test/encoder_test.rb
@@ -0,0 +1,11 @@
+require 'SimpleCov'
+SimpleCov.start
+require 'minitest/autorun'
+require 'minitest/pride'
+
+require_relative '../lib/encoder.rb'
+
+class EncoderTest < Minitest::Test
+  def it
+
+end
---
+++ test/queue_test.rb
@@ -0,0 +1,11 @@
+require 'SimpleCov'
+SimpleCov.start
+require 'minitest/autorun'
+require 'minitest/pride'
+
+require_relative '../lib/queue.rb'
+
+class QueueTest < Minitest::Test
+  def it
+
+end
