89ec429cd1232ce0a14f38a2bb5e8bb4a0f89a59
Bryan Goss <bcgoss@gmail.com>
2016-07-09 14:57:35 -0600

    Battery is dying and I want to commit

--- test/queue_test.rb
+++ test/queue_test.rb
@@ -6,6 +6,21 @@ require 'minitest/pride'
 require_relative '../lib/queue.rb'
 
 class QueueTest < Minitest::Test
-  def it
+  def setup
+    q = Queue.new
+  end
+  def test_it_pushes_items_into_queue
+    assert q.push('1')
+  end
 
+  def test_it_can_pop_items_off
+    q.push('1')
+    assert_equal '1', q.pop
+  end
+  def test_it_counts_pushed_items
+    q.push('1')
+    assert_equal 1, q.count
+    q.push('1')
+    assert_equal 2, q.count
+  end
 end
